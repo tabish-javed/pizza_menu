39fca4bfdfd4051a673b3b444ecb745e269bec16
TABISH <tabishjaved@hotmail.com>
2024-01-17 00:03:45 +0530

    css file modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
 
 const pizzaData = [
   {
@@ -49,29 +50,43 @@ const pizzaData = [
 
 function App () {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
-    </div>);
+    </div>
+  );
 }
 
 function Header () {
+  const style = { color: 'red', fontSize: '48px', textTransform: "uppercase" };
   return (
-    <header>
-      <h1>Fast React Pizza Co.</h1>
+    <header className="header">
+      <h1 style={style} >Fast React Pizza Co.</h1>
     </header>
   );
 }
 
 function Menu () {
   return (
-    <div>
+    <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, spinach, and ricotta cheese' photoName='pizzas/spinaci.jpg' price={10} />
+      <Pizza name='Pizza Funghi' ingredients='Tomato, mashrooms' price={12} photoName='pizzas/funghi.jpg'></Pizza>
+    </main>
+  );
+}
+
+
+function Pizza (props) {     // <- This is JSX
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
@@ -81,27 +96,19 @@ function Footer () {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
+  // console.log(isOpen);
 
   // if (hour >= openHour && hour <= closeHour) alert('We\'re currently open!');
   // else alert('Sorry we\'re closed!');
 
   return (
-    <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
+    <footer className="footer">{new Date().toLocaleTimeString()}. We're currently open!</footer>
   );
   // return React.createElement('footer', null, "We're currently open!");
 }
 
 
-function Pizza () {     // <- This is JSX
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
+
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
